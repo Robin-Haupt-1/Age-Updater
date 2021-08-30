7cc88bcf946f1332cf656a1323b5e296120d799c
Robin-Haupt-1 <robinhaupt@outlook.com>
2021-08-30 12:42:15 +0200

    fix #14
    also include css for hiding the "use desktop warning" by default

--- src/constants.py
+++ src/constants.py
@@ -23,53 +23,59 @@ CARD_FRONT = """\
 
 <br><br>
 <span id="birthday_notification">
-	It's their birthday today! 🎂
-<br><br></span>
+    It's their birthday today! 🎂
+    <br><br></span>
 
 <span id="use_desktop_warning">
-	⚠️ This card is outdated. Please launch Anki on your desktop computer to update your Age Updater notes.
-<br><br></span>
+    ⚠️ This card is outdated. Please launch Anki on desktop to update your Age Updater notes and then synchronize your collection.
+    <br><br></span>
 
 <script>
 
     var today = new Date();
-    var today_dd =today.getDate();
+    var today_dd = today.getDate();
     var today_mm = today.getMonth() + 1; //January is 0
-    var today_string =String(today_mm) + "-" + String(today_dd);
+    var today_string = String(today_mm) + "-" + String(today_dd);
     var today_yyyy = today.getFullYear();
     var dob = "{{text:Date of birth (YYYY-MM-DD)}}".split("-");
-	var anki_age=parseInt("{{text:Age}}");
-	
+    var anki_age = parseInt("{{text:Age}}");
+
 
     dob_mm = parseInt(dob[1])
     dob_dd = parseInt(dob[2])
-    dob_yyyy=parseInt(dob[0])
-    var dob_string = String(dob_mm) + "-" +String(dob_dd);
+    dob_yyyy = parseInt(dob[0])
+    var dob_string = String(dob_mm) + "-" + String(dob_dd);
 
     birthday_today = false;
-    
+
     if (today_string == dob_string) {
         birthday_today = true;
     }
 
     // handle leap year birthdays
-    is_leap_year =new Date(today_yyyy, 1, 29).getDate() === 29;
+    is_leap_year = new Date(today_yyyy, 1, 29).getDate() === 29;
     if (!is_leap_year && today_string == "3-1" && dob_string == "2-29") {
         birthday_today = true;
     }
     if (birthday_today) {
-        document.getElementById("birthday_notification").style.display = "block";
-    }
-    
-    // check if age in card is still current
-    var current_age= (today_yyyy - dob_yyyy) - 1
-    if ((today_mm>dob_mm)||(today_mm===dob_mm && today_dd>=dob_dd)){
-		 current_age+=1
-    }
-    if (!(current_age===anki_age)){
-        document.getElementById("use_desktop_warning").style.display = "block";
+        let element = document.getElementById("birthday_notification")
+        if (typeof (element) != 'undefined' && element != null) {
+            element.style.display = "block";
+        }
+
     }
 
+    // check if age in card is still current
+    var current_age = (today_yyyy - dob_yyyy) - 1
+    if ((today_mm > dob_mm) || (today_mm === dob_mm && today_dd >= dob_dd)) {
+        current_age += 1
+    }
+    if (!(current_age === anki_age)) {
+        let element = document.getElementById("use_desktop_warning")
+        if (typeof (element) != 'undefined' && element != null) {
+            element.style.display = "block";
+        }
+    }
 </script>\
 """  # solution for checking if present year is a leap year: https://stackoverflow.com/questions/16353211/check-if-year-is-leap-year-in-javascript
 
@@ -103,6 +109,12 @@ CARD_CSS = """\
 #birthday_notification {
     display: none;
     color: #00b1c4;
+    font-size: 20px;
+}
+
+#use_desktop_warning {
+    display: none;
+    color: #ff3030;
     font-size: 20px;
 }
 \
